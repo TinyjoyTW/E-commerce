2b6486df41c1f11dde729e337294f4cd1c3287c4
TinyjoyTW <152999162+TinyjoyTW@users.noreply.github.com>
2024-02-04 18:11:50 +0100

    delete button

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,14 +1,25 @@
-import products from "../assets/products.json";
+import productsJSON from "../assets/products.json";
 import Product from "./Product";
+import { useState } from "react";
 
 export default function Content() {
+  const [products, setProducts] = useState(productsJSON);
+
+  function deleteProduct(id) {
+    setProducts((productList) => {
+      return productList.filter((product) => {
+        return id !== product.id ? true : false;
+      });
+    });
+  }
+
   return (
     <main>
       <ul className="productList">
         {products.map((product) => {
           return (
             <li key={product.id}>
-              <Product product={product} />
+              <Product product={product} onDelete={deleteProduct} />
             </li>
           );
         })}
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,18 +1,22 @@
 import "./Product.css";
-export default function Product({ product }) {
+
+export default function Product({ product, onDelete }) {
   return (
     <article>
       <div className="productTitlePrice">
         <h2>{product.title}</h2>
         <span>{product.price}€</span>
         {product.discountPercentage > 10 ? <h3>On sale!</h3> : null}
+        <button className="deleteButton" onClick={() => onDelete(product.id)}>
+          Delete
+        </button>
       </div>
       <p>{product.description}</p>
       <p>Rating: {Math.round(product.rating * 10) / 10}</p>
       <p>Stock: {product.stock}</p>
       <p>Brand: {product.brand}</p>
       <p>Category: {product.category}</p>
-      <img className="thumbnail" src={product.thumbnail} />:
+      <img className="thumbnail" src={product.thumbnail} />
     </article>
   );
 }
